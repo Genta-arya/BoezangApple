37817b175ca76a2d6fe4ea06e83a24402590c0e9
Genta-arya <mgentaarya@gmail.com>
2024-07-15 14:45:13 +0700

    publish dummy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,16 +9,15 @@ const Footer = () => {
       <div className="lg:px-24">
         <div className="container mx-auto">
           <div className="container mx-auto flex lg:flex-row md:flex-col-reverse flex-col-reverse gap-4  md:gap-4 lg:gap-24 justify-between">
-            <div className="relative w-full">
+            <div className="relative w-full border border-gray-500  rounded-lg shadow-md">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.730950246081!2d109.96156977717112!3d-1.853445136513588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e05197a82605831%3A0xe7aa3561ca4e65ec!2sBoezang%20Apple!5e0!3m2!1sen!2sid!4v1721026662245!5m2!1sen!2sid"
                 width="100%"
                 height="450"
-                style={{ border: 0 }}
+                className="rounded-lg"
                 allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-                className="rounded-lg shadow-md"
               ></iframe>
             </div>
 
@@ -32,9 +31,9 @@ const Footer = () => {
               </h2>
               <p className="mb-4 md:text-base text-sm text-justify">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                industry. Lorem Ipsum has been the industrys standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
               </p>
               <p className="mb-4 md:text-base text-sm">
                 Jl. Pawan 1 No.54, Kauman, Kec. Benua Kayong, Kabupaten
