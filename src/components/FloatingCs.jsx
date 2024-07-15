ef7dcc4228383ef413f1327e7f7cd7f8d9303f5c
Genta-arya <mgentaarya@gmail.com>
2024-07-15 15:38:25 +0700

    publish dummy

--- src/components/FloatingCs.jsx
+++ src/components/FloatingCs.jsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { FaWhatsapp } from 'react-icons/fa'; // Import ikon WhatsApp dari react-icons
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa"; // Import ikon WhatsApp dari react-icons
 
 const FloatingCs = () => {
   return (
@@ -11,15 +11,16 @@ const FloatingCs = () => {
         href="https://wa.me/08112787771" // Ganti dengan nomor WhatsApp Anda
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-center w-16 h-16 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 transition duration-300"
+        className="flex items-center justify-center lg:w-16 lg:h-16 md:w-16 md:h-16 w-12 h-12 bg-green-500 text-white rounded-full shadow-lg hover:bg-green-600 transition duration-300"
         aria-label="Hubungi Kami di WhatsApp"
       >
-        <FaWhatsapp className="text-3xl" />
+        <FaWhatsapp className="lg:text-3xl md:text-3xl text-xl" />
       </a>
 
-      {/* Card */}
       <div className="absolute lg:-left-[12rem] -left-[10.5rem] md:-left-[12rem]  bottom-2 ml-4 bg-white border border-gray-300 shadow-lg rounded-lg p-2 max-w-xs dark:bg-gray-800 dark:border-gray-700">
-        <h3 className="text-xs font-semibold dark:text-gray-300">Butuh Bantuan?</h3>
+        <h3 className="text-xs font-semibold dark:text-gray-300">
+          Butuh Bantuan?
+        </h3>
         <p className="text-xs text-gray-700  dark:text-gray-300">
           Hubungi kami sekarang.
         </p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,24 @@
 import React from "react";
 import icon from "@/assets/icon.png";
 import Image from "next/image";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTiktok,
+  FaMapMarkedAlt, // Ikon untuk lokasi
+  FaPhoneAlt, // Ikon untuk telepon
+  FaClock, // Ikon untuk jam
+} from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-slate-100 dark:bg-black dark:text-white dark:border-gray-500  border-t py-8 px-4 md:px-8">
+    <footer className="bg-gray-100 dark:bg-black dark:text-white dark:border-gray-500 border-t py-8 px-4 md:px-8">
       <div className="lg:px-24">
         <div className="container mx-auto">
-          <div className="container mx-auto flex lg:flex-row md:flex-col-reverse flex-col-reverse gap-4  md:gap-4 lg:gap-24 justify-between">
-            <div className="relative w-full border border-gray-500  rounded-lg shadow-md">
+          <div className="container mx-auto flex lg:flex-row md:flex-col-reverse flex-col-reverse gap-4 md:gap-4 lg:gap-24 justify-between">
+            <div className="relative w-full border border-gray-500 rounded-lg shadow-md">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.730950246081!2d109.96156977717112!3d-1.853445136513588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e05197a82605831%3A0xe7aa3561ca4e65ec!2sBoezang%20Apple!5e0!3m2!1sen!2sid!4v1721026662245!5m2!1sen!2sid"
                 width="100%"
@@ -22,13 +33,41 @@ const Footer = () => {
             </div>
 
             <div className="mb-8">
-              <Image
-                src={icon}
-                className="rounded-full mb-4 md:w-32 w-20 p-1 bg-white"
-              />
-              <h2 className="md:text-2xl text-lg font-bold mb-4">
+              <div className="flex justify-center">
+                <Image
+                  src={icon}
+                  className="rounded-full mb-4 md:w-32 w-20 p-1 bg-white"
+                />
+              </div>
+              <h2 className="md:text-2xl text-lg font-bold mb-4 text-center">
                 BOEZANG APPLE
               </h2>
+              <div className="flex justify-center mb-4">
+                <a
+                  href="https://www.facebook.com/BoezangApple"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200"
+                >
+                  <FaFacebookF size={24} />
+                </a>
+                <a
+                  href="https://www.instagram.com/boezangapple/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200"
+                >
+                  <FaInstagram size={24} />
+                </a>
+                <a
+                  href="https://www.tiktok.com/@boezangapple"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mx-2 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-gray-200"
+                >
+                  <FaTiktok size={24} />
+                </a>
+              </div>
               <p className="mb-4 md:text-base text-sm text-justify">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industrys standard dummy text
@@ -36,13 +75,22 @@ const Footer = () => {
                 type and scrambled it to make a type specimen book.
               </p>
               <p className="mb-4 md:text-base text-sm">
-                Jl. Pawan 1 No.54, Kauman, Kec. Benua Kayong, Kabupaten
-                Ketapang, Kalimantan Barat 78821
+                <span className="inline-flex items-center">
+                  <FaMapMarkedAlt className="mr-2 text-2xl" /> Jl. Pawan 1 No.54, Kauman,
+                  Kec. Benua Kayong, Kabupaten Ketapang, Kalimantan Barat 78821
+                </span>
               </p>
               <p className="mb-4 md:text-base text-sm">
-                Setiap Hari : 9:30 - 22:00 WIB
+                <span className="inline-flex items-center">
+                  <FaPhoneAlt className="mr-2" /> 0896-9445-1774
+                </span>
               </p>
-              <p>0896-9445-1774</p>
+              <div className="mb-4 md:text-base text-sm">
+                <div className="inline-flex items-center">
+                  <FaClock className="mr-2" />
+                  <span>09:30 - 22:00 WIB</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
