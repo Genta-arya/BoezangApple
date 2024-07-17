6ebd12f558639dbf1852cb3a97a2eca3685c5612
Genta-arya <mgentaarya@gmail.com>
2024-07-17 14:45:18 +0700

    add quotest

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,13 +9,9 @@ import {
 } from "react-icons/fa";
 
 const Sidebar = ({ isOpen, onClose }) => {
-  const [isDarkMode, setDarkMode] = useState(false);
 
-  const toggleTheme = () => {
-    setDarkMode(!isDarkMode);
-    document.documentElement.classList.toggle("dark", !isDarkMode);
-  };
 
+  
   return (
     <div className="relative">
       <div
@@ -29,30 +25,7 @@ const Sidebar = ({ isOpen, onClose }) => {
         >
           &times;
         </button>
-        <div className="absolute top-4 right-4 flex items-center">
-          <label
-            htmlFor="theme-toggle"
-            className="flex items-center cursor-pointer"
-          >
-            <div className="relative">
-              <input
-                id="theme-toggle"
-                type="checkbox"
-                checked={isDarkMode}
-                onChange={toggleTheme}
-                className="sr-only"
-              />
-              <div className="w-7 h-4 bg-gray-300 rounded-full shadow-inner"></div>
-              <div
-                className={`absolute w-4 h-4 bg-white rounded-full shadow inset-y-0 left-0 ${
-                  isDarkMode
-                    ? "transform translate-x-4 bg-yellow-400"
-                    : "transform translate-x-0"
-                } transition-transform duration-300`}
-              ></div>
-            </div>
-          </label>
-        </div>
+       
         <div className="flex flex-col items-start mt-16 ml-5 space-y-4 text-sm">
           <a
             href="#"
