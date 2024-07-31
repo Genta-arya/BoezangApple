2d50d2f812580127f3cb7a5f3b5a12cb10a98ce1
Genta-arya <mgentaarya@gmail.com>
2024-07-31 20:48:32 +0700

    add tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -51,13 +51,32 @@ export default function RootLayout({ children }) {
           href="https://www.googletagmanager.com/gtm.js?id=G-BVZKXGQEEF"
           as="script"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-ML3R2X4C');`,
+          }}
+        />
       </Head>
       <GoogleAnalyticss />
       <Analytics />
       <GoogleTagManager gtmId="G-BVZKXGQEEF" />
       <TrackingAnalytic />
       <SpeedInsights />
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-ML3R2X4C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
