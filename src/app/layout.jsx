cff8a0f331df4094a8f65b79c0a85aef106b5cd0
Genta-arya <mgentaarya@gmail.com>
2024-07-31 21:07:44 +0700

    add tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -51,10 +51,41 @@ export default function RootLayout({ children }) {
           href="https://www.googletagmanager.com/gtm.js?id=G-BVZKXGQEEF"
           as="script"
         />
-            <link
+        <link
           rel="preload"
           href="https://www.googletagmanager.com/gtm.js?id=GTM-ML3R2X4C"
           as="script"
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function() {
+              window.sib = {
+                equeue: [],
+                client_key: "4at68bx4o6mh9d6p50in3p4m"
+              };
+              /* OPTIONAL: email for identify request*/
+              // window.sib.email_id = 'example@domain.com';
+              window.sendinblue = {};
+              for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+                (function(k) {
+                  window.sendinblue[k] = function() {
+                    var arg = Array.prototype.slice.call(arguments);
+                    (window.sib[k] || function() {
+                      var t = {};
+                      t[k] = arg;
+                      window.sib.equeue.push(t);
+                    })(arg[0], arg[1], arg[2]);
+                  };
+                })(j[i]);
+              }
+              var n = document.createElement("script"),
+                  i = document.getElementsByTagName("script")[0];
+              n.type = "text/javascript", n.id = "sendinblue-js",
+              n.async = !0, n.src = "https://sibautomation.com/sa.js?key="+ window.sib.client_key,
+              i.parentNode.insertBefore(n, i), window.sendinblue.page();
+            })();`,
+          }}
         />
         <script
           dangerouslySetInnerHTML={{
